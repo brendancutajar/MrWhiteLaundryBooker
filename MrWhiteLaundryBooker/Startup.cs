1afbf125119c2820cbedaa8bd1e4141dba069c09
bcutajar <bc19890@gmail.com>
2017-01-14 07:30:42 +0100

    Add project files.

---
+++ MrWhiteLaundryBooker/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MrWhiteLaundryBooker.Startup))]
+namespace MrWhiteLaundryBooker
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
